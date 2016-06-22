5ad66e875b07131a440566b0e38bbb8fb7bdc807
Oscar Barillas <oscarbarillas93@gmail.com>
2016-06-21 20:14:48 -0700

    Prep Repo for Heroku

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 var express = require('express');
-// Create our app
+
+const PORT = process.env.PORT || 3000;
+
 var app = express();
+
+app.use(function(req, res, next){
+  if (req.headers['x-forward-proto' === 'http']){
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 app.use(express.static('public'));
-app.listen(3001, function () {
-  console.log('Express server is up on port 3001');
+app.listen(PORT, function () {
+  console.log('Express server is up on port' + PORT);
 });
