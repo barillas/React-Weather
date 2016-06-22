0f83651ddb609b502a8b7bd4b226eda4565e5aee
Oscar Barillas <oscarbarillas93@gmail.com>
2016-06-21 23:09:30 -0700

    Centered Elements and initiated foundation grid system

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,24 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 
 var Examples = (props) => {
   return (
 
     <div>
-      <h3>Examples</h3>
+      <h1 className = "text-center">Examples</h1>
+      <p>Here are a few locations to try out</p>
+      <ol>
+        <li>
+          <Link to="/?Miami">Miami</Link>
+
+        </li>
+
+        <li>
+          <Link to="/?Los%Angeles">Los Angeles</Link>
+        </li>
+
+      </ol>
     </div>
 
   )
--- public/bundle.js
+++ public/bundle.js
@@ -24858,11 +24858,14 @@
 	    null,
 	    React.createElement(Nav, null),
 	    React.createElement(
-	      'h2',
-	      null,
-	      ' Main Component'
-	    ),
-	    props.children
+	      'div',
+	      { className: 'row' },
+	      React.createElement(
+	        'div',
+	        { className: 'columns medium-6 large-4 small-centered' },
+	        props.children
+	      )
+	    )
 	  );
 	};
 
@@ -26396,14 +26399,46 @@
 
 	var React = __webpack_require__(8);
 
+	var _require = __webpack_require__(166);
+
+	var Link = _require.Link;
+
+
 	var Examples = function Examples(props) {
 	  return React.createElement(
 	    'div',
 	    null,
 	    React.createElement(
-	      'h3',
-	      null,
+	      'h1',
+	      { className: 'text-center' },
 	      'Examples'
+	    ),
+	    React.createElement(
+	      'p',
+	      null,
+	      'Here are a few locations to try out'
+	    ),
+	    React.createElement(
+	      'ol',
+	      null,
+	      React.createElement(
+	        'li',
+	        null,
+	        React.createElement(
+	          Link,
+	          { to: '/?Miami' },
+	          'Miami'
+	        )
+	      ),
+	      React.createElement(
+	        'li',
+	        null,
+	        React.createElement(
+	          Link,
+	          { to: '/?Los%Angeles' },
+	          'Los Angeles'
+	        )
+	      )
 	    )
 	  );
 	};
